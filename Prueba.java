4f97842de7b4c5f9a23e6cbf6dc98d2aa9ad2323
Dani <ebaudani@gmail.com>
2024-09-19 19:12:35 +0200

    he puesto mi comida favorita

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ public class Prueba{
     public static void main(String[] args) {
         System.out.println("Polanco Mena");
         System.out.println("Tengo 19 años y nací en República Dominicana");
+        System.out.println("Mi comida favorita es el durum Kebab");
     }
 }
