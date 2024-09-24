51547addd6a246a763fa9d07f60522e98d330f6f
Dani <ebaudani@gmail.com>
2024-09-24 08:54:27 +0200

    Aañadimos cantante favorito

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba{
     public static void main(String[] args) {
-        System.out.println("Polanco Mena");
         System.out.println("Tengo 19 años y nací en República Dominicana");
         System.out.println("Mi comida favorita es el durum Kebab");
+        System.out.println("MI cantante favorito es ed sheeran");
     }
 }
